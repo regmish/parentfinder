336c11ac4d2c19534908ad14a39af8e8b797a563
Regmi Shank <shankarregmi@gmail.com>
2016-04-05 16:42:45 +0545

    adds adding person function

--- parentfinder.js
+++ parentfinder.js
@@ -16,29 +16,42 @@ var collection = [
  * @param  {String} person Name of a Person whose parent't is to be found
  * @return {String}      Grand-grandfather of that person
  */
-function findParent(person) {
-    var _current = collection.filter(function(node) {
-        return node.name == person
-    })[0];
+var parentFinder = {
+	findParent: function findParent(person) {
+	    var _current = collection.filter(function(node) {
+	        return node.name == person
+	    })[0];
 
-    if(!_current) { // CASE : No matching name found
-    	return 'person not in the collection';
-    }
-    else if (_current && !_current.parent) { // CASE: No parent found
-        return _current.name;
-    }
-    return findParent(_current.parent); // find parent's parent again
-};
+	    if(!_current) { // CASE : No matching name found
+	    	return 'person not in the collection';
+	    }
+	    else if (_current && !_current.parent) { // CASE: No parent found
+	        return _current.name;
+	    }
+	    return findParent(_current.parent); // find parent's parent again
+	},
+	addPerson : function(personObj) {
+		if('object' === typeof personObj && personObj.name) {
+			collection.push(personObj);
+			console.log(personObj.name + ' added');
+		}
+		else {
+			console.log('Cannot add person');
+		}
+	}
+}
 
 
 
-module.exports = findParent;
+module.exports = parentFinder;
 
 
 //-- Test Code ----------------------------------------------------------
 if (require.main === module) {
     (function() {
     	var name = process.argv[2] || 'Ram';
-        console.log(findParent(name));
+				// add one personObj
+				parentFinder.addPerson({name:'Saurav', parent:'Hari'});
+        console.log(parentFinder.findParent(name));
     })();
 }
